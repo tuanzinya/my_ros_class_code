e2ab6664de45a8870bab7869f4a29beb2a9eed87
tuanzinya <ztuan2186@gmail.com>
2026-04-09 19:30:51 +0800

    Update tag_grab.cpp

--- lqc_ros_class_code/src/visual_indetity/src/tag_grab.cpp
+++ lqc_ros_class_code/src/visual_indetity/src/tag_grab.cpp
@@ -17,39 +17,28 @@ int main(int argc, char **argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
     ros::NodeHandle nh;
-
-
-
     // 使用到的 service
     ros::ServiceClient arm_move_open_client = nh.serviceClient<upros_message::ArmPosition>("/upros_arm_control/arm_pos_service_open");
-    
     ros::ServiceClient arm_zero_client = nh.serviceClient<std_srvs::Empty>("/upros_arm_control/zero_service");
     ros::ServiceClient arm_grab_client = nh.serviceClient<std_srvs::Empty>("/upros_arm_control/grab_service");
     ros::ServiceClient arm_release_client = nh.serviceClient<std_srvs::Empty>("/upros_arm_control/release_service");
-
     tf2_ros::Buffer buffer;
     tf2_ros::TransformListener listener(buffer);
     ROS_INFO("tf coordinate transformaing....");
 
     // 获取tag到机械臂基坐标的坐标变换
     geometry_msgs::TransformStamped tfs_1 = buffer.lookupTransform("arm_base_link", "tag_1", ros::Time(0), ros::Duration(100));
-
     // 单位转换，ros坐标系到逆运算坐标系
     int x = -int(tfs_1.transform.translation.y * 1000);
     int y = int(tfs_1.transform.translation.x * 1000) + 30;
     int z = int(tfs_1.transform.translation.z * 1000 + 40);
-    
     ROS_INFO("Target X: %d, Target Y: %d, Target Z: %d", x, y, z);
     std_srvs::Empty empty_srv;
-    
     // 第一步，打开夹爪
     arm_release_client.call(empty_srv);
     sleep(5.0);
-
-// 在调用服务之前，加这两行！！！
-arm_move_open_client.waitForExistence();
-ros::Duration(0.5).sleep();
-
+    arm_move_open_client.waitForExistence();
+    ros::Duration(0.5).sleep();
     // 第二步，运动到抓取位置
     upros_message::ArmPosition move_srv;
     move_srv.request.x = x;
@@ -57,11 +46,9 @@ ros::Duration(0.5).sleep();
     move_srv.request.z = z;
     arm_move_open_client.call(move_srv);
     sleep(5.0);
-
     // 第三步，闭合夹爪
     arm_grab_client.call(empty_srv);
     sleep(5.0);
-
     // 第四步，返回零位
     arm_zero_client.call(empty_srv);
     sleep(5.0);
